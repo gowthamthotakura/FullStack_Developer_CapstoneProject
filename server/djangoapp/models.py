98a51ce765de328b2481fe0b915446d1df612503
Gowtham Thotakura <174505325+gowthamthotakura@users.noreply.github.com>
2024-10-10 16:42:19 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -35,6 +35,6 @@ class CarModel(models.Model):
   ])
     # Other fields as needed
 
-    def __str__(self):
-        return self.name
+  def __str__(self):
+      return self.name
 # Return the name as the string representation
