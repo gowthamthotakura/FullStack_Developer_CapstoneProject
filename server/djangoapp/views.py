9da7eced0d7a6e12582f0d4b736af3dd145daca5
Gowtham Thotakura <174505325+gowthamthotakura@users.noreply.github.com>
2024-10-10 17:19:01 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -8,16 +8,14 @@ from django.contrib.auth import logout
 # from django.contrib import messages
 # from datetime import datetime
 
-from .models import CarMake, CarModel
-
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
-
-# from .restapis import get_request, analyze_review_sentiments, post_review
+from .models import CarMake, CarModel
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 
 # Get an instance of a logger
@@ -25,6 +23,7 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
+
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
 def login_user(request):
@@ -63,46 +62,29 @@ def registration(request):
     username_exist = False
     # email_exist = False
     try:
-        # Check if user already exists
+        # check if user already exists
         User.objects.get(username=username)
-        username_exist = True
-
+        # username_exists = True
     except Exception:
-        # If not, simply log this is a new user
+        # if not, simply log this is a new user
         logger.debug("{} is new user".format(username))
 
-    # If it is a new user
+    # if it is a new user
     if not username_exist:
-        # Create user in auth_user table
+        # create user in auth_user table
         user = User.objects.create_user(
             username=username,
             first_name=first_name,
             last_name=last_name,
             password=password,
             email=email)
-        # Login the user and redirect to list page
+        # login the user and redirect to list page
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
     else:
         data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
-
-
-# Update the `get_dealerships` render list of dealerships all by default,
-# particular state if state is passed.
-def get_dealerships(request, state="All"):
-    if (state == "All"):
-        endpoint = "/fetchDealers"
-    else:
-       endpoint = "/fetchDealers/"+state
-    dealerships = get_request(endpoint)
-    return JsonResponse({"status": 200, "dealers": dealerships})
-
-# # Update the `get_dealerships` view to render the index page with
-# a list of dealerships
-# def get_dealerships(request):
-# ...
 
 
 def get_cars(request):
@@ -117,11 +99,19 @@ def get_cars(request):
                      "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
+
+# # Update the `get_dealerships` view to render the index page with
+# a list of dealerships
+def get_dealerships(request, state="All"):
+    if (state == "All"):
+        endpoint = "/fetchDealers"
+    else:
+        endpoint = "/fetchDealers/"+state
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status": 200, "dealers": dealerships})
+
+
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
-# ...
-
-
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
     if (dealer_id):
@@ -135,10 +125,8 @@ def get_dealer_reviews(request, dealer_id):
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
 
+
 # Create a `get_dealer_details` view to render the dealer details
-# def get_dealer_details(request, dealer_id):
-
-
 def get_dealer_details(request, dealer_id):
     if (dealer_id):
         endpoint = "/fetchDealer/"+str(dealer_id)
@@ -146,11 +134,9 @@ def get_dealer_details(request, dealer_id):
         return JsonResponse({"status": 200, "dealer": dealership})
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
+
+
 # Create a `add_review` view to submit a review
-# def add_review(request):
-# ...
-
-
 def add_review(request):
     if (request.user.is_anonymous is False):
         data = json.loads(request.body)
