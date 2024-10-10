9f2eea52e3d4fb3895f30ed2e2868a3aa6749be0
Gowtham Thotakura <174505325+gowthamthotakura@users.noreply.github.com>
2024-10-10 16:32:55 +0530

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -19,7 +19,7 @@ sentiment_analyzer_url = os.getenv(
 def get_request(endpoint, ** kwargs):
     params = ""
     if (kwargs):
-        for key, value in kwargs.items(): 
+        for key, value in kwargs.items():
             params = params+key + "=" + value + "&"
 
     request_url = backend_url+endpoint+"?"+params
@@ -50,6 +50,8 @@ def analyze_review_sentiments(text):
         print(f"Network exception occurred")
 # def post_review(data_dict):
 # Add code for posting review
+
+
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
