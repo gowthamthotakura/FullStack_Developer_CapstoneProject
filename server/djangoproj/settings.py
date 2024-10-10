9de07e3fa30896f8df56de7575ef1bb4ffcf5c8e
Gowtham Thotakura <174505325+gowthamthotakura@users.noreply.github.com>
2024-10-10 16:59:17 +0530

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -103,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME':
             'django.contrib.auth.password_validation.'
             'UserAttributeSimilarityValidator'
-        ),
     },
     {
         'NAME':
