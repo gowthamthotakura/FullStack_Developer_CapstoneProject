0a37e0134ac55da569c59130d92b878064c7c443
Gowtham Thotakura <174505325+gowthamthotakura@users.noreply.github.com>
2024-10-10 16:57:38 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -17,17 +17,17 @@ class CarMake(models.Model):
     description = models.TextField()
     # Other fields as needed
 
-    def __str__(self):
-      return self.name
+def __str__(self):
+    return self.name
     # Return the name as the string representation
 
 class CarModel(models.Model):
-  car_make = models.ForeignKey(CarMake,on_delete=models.CASCADE)
-  name = models.CharField(max_length=100)
-  CAR_TYPES = [
-    ('SEDAN', 'Sedan'),
-    ('SUV', 'SUV'),
-    ('WAGON', 'Wagon'),
+    car_make = models.ForeignKey(CarMake,on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
   ]
   type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
   year = models.IntegerField(default=2023, validators=[
