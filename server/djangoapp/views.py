c691a3183e61ecae9793424358201cb7adae9a32
Gowtham Thotakura <174505325+gowthamthotakura@users.noreply.github.com>
2024-10-10 13:31:13 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,12 +1,12 @@
 # Uncomment the required imports before adding the code
 
-from django.shortcuts import render
+# from django.shortcuts import render
 # from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 # from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
 # from django.contrib import messages
-from datetime import datetime
+# from datetime import datetime
 
 from .models import CarMake, CarModel
 
@@ -52,7 +52,7 @@ def logout_request(request):
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-    context = {}
+    # context = {}
 
     data = json.loads(request.body)
     username = data['userName']
@@ -61,13 +61,13 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    email_exist = False
+    # email_exist = False
     try:
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
 
-    except Exception as e:
+    except Exception:
         # If not, simply log this is a new user
         logger.debug("{} is new user".format(username))
 
@@ -75,7 +75,8 @@ def registration(request):
     if not username_exist:
         # Create user in auth_user table
         user = User.objects.create_user(username=username,
-               first_name=first_name, last_name=last_name, password=password,
+               first_name=first_name, last_name=last_name,
+               password=password,
                email=email),
         # Login the user and redirect to list page
         login(request, user)
@@ -84,11 +85,11 @@ def registration(request):
     else:
         data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
-        
+
 
 # Update the `get_dealerships` render list of dealerships all by default,
 # particular state if state is passed.
-def get_dealerships(request, state= "All"):
+def get_dealerships(request, state = "All"):
     if (state == "All"):
         endpoint = "/fetchDealers"
     else:
@@ -110,7 +111,8 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name,
+                     "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
@@ -131,6 +133,8 @@ def get_dealer_reviews(request, dealer_id):
 
 # Create a `get_dealer_details` view to render the dealer details
 # def get_dealer_details(request, dealer_id):
+
+
 def get_dealer_details(request, dealer_id):
     if (dealer_id):
         endpoint = "/fetchDealer/"+str(dealer_id)
@@ -141,6 +145,8 @@ def get_dealer_details(request, dealer_id):
 # Create a `add_review` view to submit a review
 # def add_review(request):
 # ...
+
+
 def add_review(request):
     if (request.user.is_anonymous == False):
         data = json.loads(request.body)
