727a959c0838b3c58a8b42aa00d4a2d7b54b088b
Gowtham Thotakura <174505325+gowthamthotakura@users.noreply.github.com>
2024-10-10 16:08:23 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -74,11 +74,12 @@ def registration(request):
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username,
-                          first_name=first_name,
-                          last_name=last_name,
-                          password=password,
-                          email=email)
+        user = User.objects.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+            email=email)
         # Login the user and redirect to list page
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
@@ -154,11 +155,14 @@ def add_review(request):
     if (request.user.is_anonymous == False):
         data = json.loads(request.body)
         try:
-            # response = post_review(data)
+            post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
             return JsonResponse({"status": 401, "message":
-                                 "Error in posting review"})
+                                 "message": "Error in posting review"})
+        finally:
+            print("add_review request successful!")
     else:
-        return JsonResponse({"status": 403, "message":
+        return JsonResponse({"status": 403,
+                             "message": "Unauthorized"})
                              "Unauthorized"})
