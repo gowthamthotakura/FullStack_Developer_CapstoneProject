5ce1d6997aa7eab3f3e2f21d5aa474df7a292f11
Gowtham Thotakura <174505325+gowthamthotakura@users.noreply.github.com>
2024-10-10 15:43:41 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -75,10 +75,10 @@ def registration(request):
     if not username_exist:
         # Create user in auth_user table
         user = User.objects.create_user(username=username,
-                                        first_name=first_name,
-                                        last_name=last_name,
-                                        password=password,
-                                        email=email)
+                          first_name=first_name,
+                          last_name=last_name,
+                          password=password,
+                          email=email)
         # Login the user and redirect to list page
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
@@ -94,7 +94,7 @@ def get_dealerships(request, state="All"):
     if (state == "All"):
         endpoint = "/fetchDealers"
     else:
-        endpoint = "/fetchDealers/"+state
+       endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
     return JsonResponse({"status": 200, "dealers": dealerships})
 
